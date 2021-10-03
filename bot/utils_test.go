23ed31c5829a5af3848cdc9a36d4f9bd7c50ed7d
Davide Riva <driva95@protonmail.com>
2021-10-03 20:27:28 +0000

    Code coverage increased

--- bot/utils_test.go
+++ bot/utils_test.go
@@ -1,6 +1,7 @@
 package bot
 
 import "testing"
+import _ "embed"
 
 func TestMakeQuery(t *testing.T) {
 	token := "dummy-token-placeholder"
@@ -23,5 +24,16 @@ func TestMakeQueryFile(t *testing.T) {
 	groundTruth := "https://api.telegram.org/file/bot" + token + "/" + filePath
 	if query != groundTruth {
 		t.Fatalf("'%s' should be equal to '%s'", query, groundTruth)
+	}
+}
+
+//go:embed testdata/base_response.json
+var base_response_fake_data []byte
+
+func TestIsResponseOk(t *testing.T) {
+	err := isResponseOk(base_response_fake_data)
+
+	if err != nil {
+		t.Fatalf("No errors expected, returned %s", err.Error())
 	}
 }
