6cdcae676167b14fefadf0858d023fd78fa1a4be
Davide Riva <driva95@protonmail.com>
2021-10-03 12:59:28 +0000

    Fix go:embed cannot used inside func

--- bot/fake/bot.go
+++ bot/fake/bot.go
@@ -1,11 +1,16 @@
 package fake
 
+import _ "embed"
+
 type Bot struct{}
 
 func (bot Bot) GetError() error {
 	return nil
 }
 
+//go:embed dummy_responses/get_updates.json
+var getUpdatesBody []byte
+
 func (bot Bot) GetUpdates(limit int, timeout int, allowedUpdates []string) {
-	return
+	_ = getUpdatesBody
 }
