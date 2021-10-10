aaa67607bc88ecf4f537a23558d3a58f05873ab1
Davide Riva <driva95@protonmail.com>
2021-10-10 20:30:44 +0000

    IP should be set as a parameter

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-ssh root@10.11.99.1 'mkdir -p /opt/remarkabot && mkdir -p $HOME/.config/remarkabot' && \
-scp ./remarkabot root@10.11.99.1:/opt/remarkabot/remarkabot && \
-scp ./systemd/remarkabot.service root@10.11.99.1:/etc/systemd/user/remarkabot.service && \
-scp ./systemd/remarkabot.timer root@10.11.99.1:/etc/systemd/user/remarkabot.timer && \
-scp ./systemd/.env root@10.11.99.1:$HOME/.config/remarkabot/.env
+ssh root@$1 'mkdir -p /opt/remarkabot && mkdir -p $HOME/.config/remarkabot' && \
+scp ./remarkabot root@$1:/opt/remarkabot/remarkabot && \
+scp ./systemd/remarkabot.service root@$1:/etc/systemd/user/remarkabot.service && \
+scp ./systemd/remarkabot.timer root@$1:/etc/systemd/user/remarkabot.timer && \
+scp ./systemd/.env root@$1:$HOME/.config/remarkabot/.env
