f6fd1ce59a39b2b17b740cdffae2abe194eadb99
Davide Riva <davide.riva@nttdata.com>
2021-10-11 21:24:07 +0200

    better error handling and notifications

--- main.go
+++ main.go
@@ -30,8 +30,8 @@ func main() {
 
 	bot := bot.GetBot(telegramToken)
 
-	const maxUpdates = 10
-	for updates := bot.GetUpdates(maxUpdates); len(updates) > 0; updates = bot.GetUpdates(maxUpdates) {
+	const maxConcUpdates = 1
+	for updates := bot.GetUpdates(maxConcUpdates); len(updates) > 0; updates = bot.GetUpdates(maxConcUpdates) {
 		logger.Info("Fetching new updates")
 
 		for _, update := range updates {
@@ -41,26 +41,29 @@ func main() {
 			chat := message.Chat
 
 			if chat.Type == "private" {
+				bot.Commit(update.UpdateId)
+
 				logger.Warn("Private messages received and discarded", zap.Int64("id", message.MessageId))
 				bot.SendMessage(chat.Id, message.MessageId, "Hey!\nAdd me to a group and send me documents to see them in your Remarkable!")
-				bot.Commit(update.UpdateId)
 				continue
 			}
 
 			document := message.Document
 
 			if document.FileId == "" {
-				logger.Error("Message does not contain a document", zap.Int64("id", message.MessageId))
-				bot.SendMessage(chat.Id, message.MessageId, "You can send me only documents")
 				bot.Commit(update.UpdateId)
+
+				logger.Warn("Message does not contain a document", zap.Int64("id", message.MessageId))
+				bot.SendMessage(chat.Id, message.MessageId, "You can send me only documents")
 				continue
 			}
 
 			mime := document.MimeType
 			if mime != "application/pdf" && mime != "application/epub+zip" {
-				logger.Error("Document is not a PDF or epub", zap.String("id", document.FileId))
-				bot.SendMessage(message.Chat.Id, message.MessageId, "Document is not a PDF or epub")
 				bot.Commit(update.UpdateId)
+
+				logger.Warn("Document is not a PDF or epub", zap.String("id", document.FileId))
+				bot.SendMessage(message.Chat.Id, message.MessageId, "Document is not a PDF or epub")
 				continue
 			}
 
@@ -75,6 +78,12 @@ func main() {
 			}
 
 			bot.Commit(update.UpdateId)
+
+			bot.SendMessage(
+				message.Chat.Id,
+				message.MessageId,
+				"Document received, reboot your device to see it",
+			)
 		}
 
 	}
