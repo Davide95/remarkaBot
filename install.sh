f5044db65f0d5c0936799fc549929729c04b686e
Davide Riva <driva95@protonmail.com>
2021-10-11 16:23:13 +0000

    Fix: home var is locally and points to the wrong dir

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ ssh root@$1 'mkdir -p /opt/remarkabot && mkdir -p $HOME/.config/remarkabot'
 scp ./remarkabot root@$1:/opt/remarkabot/remarkabot
 scp ./systemd/remarkabot.service root@$1:/etc/systemd/user/remarkabot.service
 scp ./systemd/remarkabot.timer root@$1:/etc/systemd/user/remarkabot.timer
-scp ./systemd/.env root@$1:$HOME/.config/remarkabot/.env
+scp ./systemd/.env root@$1:/home/root/.config/remarkabot/.env
