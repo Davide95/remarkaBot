9713e8226ef0dac46803da9c7eecc8ad610947bc
Davide Riva <driva95@protonmail.com>
2021-10-11 16:44:51 +0000

    Fix: refresh and start of the service should be done in place

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eux
 
-go build .
+GOOS="linux" GOARCH="arm" GOARM="7" go build .
 ssh root@$1 'mkdir -p /opt/remarkabot && mkdir -p $HOME/.config/remarkabot'
 scp ./remarkabot root@$1:/opt/remarkabot/remarkabot
 scp ./systemd/remarkabot.service root@$1:/etc/systemd/system/remarkabot.service
 scp ./systemd/remarkabot.timer root@$1:/etc/systemd/system/remarkabot.timer
 scp ./systemd/.env root@$1:/home/root/.config/remarkabot/.env
-ssh root@$1 'systemctl enable remarkabot.timer'
+ssh root@$1 'systemctl daemon-reload && systemctl enable --now remarkabot.timer'
