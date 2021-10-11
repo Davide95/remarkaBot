0fe5f783010f515de9d1d2ca83a6afb2e99be8b8
Davide Riva <driva95@protonmail.com>
2021-10-11 16:37:17 +0000

    Fix: systemd installation missing

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -eux
 go build .
 ssh root@$1 'mkdir -p /opt/remarkabot && mkdir -p $HOME/.config/remarkabot'
 scp ./remarkabot root@$1:/opt/remarkabot/remarkabot
-scp ./systemd/remarkabot.service root@$1:/etc/systemd/user/remarkabot.service
-scp ./systemd/remarkabot.timer root@$1:/etc/systemd/user/remarkabot.timer
+scp ./systemd/remarkabot.service root@$1:/etc/systemd/system/remarkabot.service
+scp ./systemd/remarkabot.timer root@$1:/etc/systemd/system/remarkabot.timer
 scp ./systemd/.env root@$1:/home/root/.config/remarkabot/.env
+ssh root@$1 'systemctl enable remarkabot.timer'
