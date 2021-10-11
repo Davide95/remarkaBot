3c1ea559e40fb911f9caa5e3a6ca4f6c94b34157
Davide Riva <davide.riva@nttdata.com>
2021-10-11 21:12:00 +0200

    Speed up grep

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if fgrep -q "wifion=true" /etc/remarkable.conf; then
+if LC_ALL=C fgrep -q "wifion=true" /etc/remarkable.conf; then
     /opt/remarkabot/remarkabot
 else
     echo "WiFi is not enabled"
