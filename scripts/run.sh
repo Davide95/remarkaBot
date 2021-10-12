38662b9f0a67447f9c6ecbbd39de2ea293325338
Davide Riva <davide.riva@nttdata.com>
2021-10-12 23:21:34 +0200

    GOGC disabled in the run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if LC_ALL=C grep -F "wifion=true" /etc/remarkable.conf; then
-    /opt/remarkabot/remarkabot
+    GOGC=off /opt/remarkabot/remarkabot
 else
     echo "WiFi is not enabled"
 fi
