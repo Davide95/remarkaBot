fdde06e02e6d98b8e390845c681f169bd26fd820
Davide Riva <davide.riva@nttdata.com>
2021-10-15 23:05:01 +0200

    Fix: if configs are commented, they should be ignored

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if LC_ALL=C grep -F "wifion=true" /etc/remarkable.conf; then
+if LC_ALL=C grep -F -x "wifion=true" /etc/remarkable.conf; then
     GOGC=off /opt/remarkabot/remarkabot
 else
     echo "WiFi is not enabled"
